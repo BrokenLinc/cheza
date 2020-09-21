f6a8fd96346703a07c4fd57e9ca60a4d3eed9efe
Lincoln Anderson <landerson@threefivetwo.com>
2020-09-20 20:55:01 -0400

    initial commit

---
+++ src/App.js
@@ -0,0 +1,54 @@
+import React from 'react';
+import { Flex, Image, Grid } from '@chakra-ui/core';
+import { identity, map, shuffle, times } from 'lodash';
+
+import ThemeProvider from './ThemeProvider';
+
+const getShuffledSymbols = (count) => shuffle(times(count, identity));
+
+const App = () => {
+  const count = 4;
+  const columns = Math.ceil(Math.sqrt(count));
+  const [symbols, setSymbols] = React.useState(getShuffledSymbols(count));
+  const [tapped, setTapped] = React.useState(0);
+  const reset = () => {
+    setSymbols(getShuffledSymbols(count));
+    setTapped(0);
+  };
+  const handleImageMouseDown = (index) => {
+    // Force source order this this line:
+    if (symbols[index] !== tapped) return;
+
+    setTapped((n) => n + 1);
+  };
+  React.useEffect(() => {
+    let resetTimeout;
+    if (tapped >= count) {
+      resetTimeout = setTimeout(reset, 1000);
+    }
+    return () => {
+      clearTimeout(resetTimeout);
+    };
+  }, [tapped]);
+  return (
+    <ThemeProvider>
+      <Flex alignItems="center" justifyContent="center" flexGrow="1">
+        <Flex width="100vmin" height="100vmin" alignItems="center">
+          <Grid gridTemplateColumns={times(columns, () => '1fr').join(' ')} gridGap="5vmin" margin="10vmin">
+            {map(symbols, (symbol, index) => (
+              <Image
+                key={symbol}
+                borderRadius="50%"
+                src={`./symbols/disc-${symbol + 1}.png`}
+                visibility={tapped > symbol ? 'hidden' : 'visible'}
+                onMouseDown={() => handleImageMouseDown(index)}
+              />
+            ))}
+          </Grid>
+        </Flex>
+      </Flex>
+    </ThemeProvider>
+  );
+};
+
+export default App;
